af739343f3892e3078598c3891bf97c7f85e653a
David Murray <davidmurray11@gmail.com>
2021-06-07 12:21:09 +0100

    update call to Lorem.words
    old style is deprecated

--- spec/factories/downloads.rb
+++ spec/factories/downloads.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :download do
     comment      { Faker::Lorem.sentence.truncate(80) }
     data         { Faker::Lorem.paragraphs }
-    file_name    { Faker::Lorem.words(1).first + ".txt" }
+    file_name    { Faker::Lorem.words(number: 1).first + ".txt" }
     content_type { "text/plain" }
   end
 end
