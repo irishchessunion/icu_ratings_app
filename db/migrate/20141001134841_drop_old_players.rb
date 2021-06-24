0a02a70790dfeaec1a90c0699b0e849ae967478d
David Murray <davidmurray11@gmail.com>
2021-06-24 15:46:42 +0100

    remove two database migrations that are breaking

--- db/migrate/20141001134841_drop_old_players.rb
+++
@@ -1,9 +0,0 @@
-class DropOldPlayers < ActiveRecord::Migration[4.2]
-  def up
-    drop_table :old_players
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration, "Old player data is now in www_production.players (status: inactive, source: archive)"
-  end
-end
