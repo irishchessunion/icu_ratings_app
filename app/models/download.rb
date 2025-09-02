7876d976aad302fd0ae798893e0c4fa3f6d94249
murrayben <dayintoknight@gmail.com>
2025-09-02 15:57:02 +0000

    added CSV generator for player lists in Swiss Manager format

--- app/models/download.rb
+++ app/models/download.rb
@@ -38,4 +38,66 @@ class Download < ApplicationRecord
     matches = includes(:rating_list).order(updated_at: :desc)
     paginate(matches, path, params)
   end
+
+  def self.rating_list_csv(list)
+    year = Time.now.year
+    month = Time.now.month
+    if month < 8
+      year -= 1
+    end
+
+    this_season = "#{year}-#{(year+1).to_s[-2..]}"
+    last_season = "#{year-1}-#{(year).to_s[-2..]}"
+
+    last_list = IcuRating.maximum("list")
+    tournament_cut_off = RatingList.where("date = ?", last_list).first().tournament_cut_off
+    last_tournament_rorder = Tournament.where("finish <= ? AND stage = 'rated' AND rorder IS NOT NULL",
+      tournament_cut_off).maximum("rorder")
+    icu_players = IcuPlayer.where("deceased = 0 AND master_id IS NULL")
+    subscriptions = Subscription.where("season IN (?, ?) OR category = 'lifetime'", this_season, last_season)
+    ratings = Player.select(:icu_id, :new_rating)
+      .where("icu_id IS NOT NULL AND new_rating IS NOT NULL")
+      .joins(:tournament)
+    
+    if list == 'published'
+      ratings = ratings.where("rorder <= ?", last_tournament_rorder)
+    elsif list == 'live'
+      ratings = ratings.where("rorder IS NOT NULL")
+    else 
+      raise "unknown list"
+    end
+    ratings = ratings.order(rorder: :desc)
+    
+    rows = []
+    for player in icu_players
+      title = case player.title
+        when nil, '' then ''
+        when 'GM' then 'g'
+        when 'IM' then 'i'
+        when 'FM' then 'f'
+        when 'CM' then 'c'
+        else '?'
+        end
+      latest_rating = ratings.where(icu_id: player.id).first()&.new_rating
+      
+      sub = 'L'
+      sub = case subscriptions.where(icu_id: player.id).order(season: :desc).first()&.season
+        when this_season then 'S'
+        when last_season then 'P'
+        else 'U'
+        end unless not subscriptions.where(icu_id: player.id, category: 'lifetime').first().nil?
+      sex = player.gender == "F" ? "F": ""
+      dob = player.dob ? Date.new(player.dob.year, 1, 1) : '1900/01/01'
+      rows << [player.id, player.name, title, player.fed, latest_rating, 0, dob, sub, sex, player.club]
+    end
+
+    rows.sort_by! {|e| e[1]}
+
+    CSV.generate do |csv| 
+      csv << %w(ID_no Name Title Fed Rtg_Nat Games Birthday Sub Sex ClubName)
+      for row in rows
+        csv << row
+      end
+    end
+  end
 end
--- lib/icu/export.rb
+++ lib/icu/export.rb
@@ -23,29 +23,18 @@ module ICU
           file  = "tmp/#{short}.zip"
           match = "wrote #{file}"
           if out.index(match)
-            comment      = "Latest #{type} ratings"
-            file_name    = "#{short}_ratings.zip"
-            content_type = "application/zip"
-            if type == "published"
-              rating_list = RatingList.first
-            else
-              rating_list = nil
-            end
-            data         = File.open(file, "r", encoding: "ASCII-8BIT") { |f| f.read }
-            download = Download.where(comment: comment, file_name: file_name, content_type: content_type, rating_list: rating_list).first
-            if download
-              action = "updated"
-              download.data = data
-              download.save!
-            else
-              action = "created"
-              download = Download.create!(comment: comment, file_name: file_name, content_type: content_type, rating_list: rating_list, data: data)
-            end
+            action = self.write_to(short, type, file, 'zip')
             report.push "#{action} #{type} ratings download"
           else
             report.push "output doesn't match '#{match}'"
             success = false
           end
+
+          file = "tmp/#{short}.csv"
+          csv_text = Download.rating_list_csv(type)
+          File.write(file, csv_text)
+          action = self.write_to(short, type, file, 'csv')
+          report.push "#{action} #{type} ratings download (csv)"
         end
       rescue => e
         report.push "EXCEPTION: #{e.message}"
@@ -55,6 +44,32 @@ module ICU
 
       # Create an Event to summarize what happened.
       Event.create(name: "Ratings Export", report: report.join("\n"), time: Time.now - @start, success: success)
+    end
+    
+    def self.write_to(short, type, file, extension)
+        comment      = "Latest #{type} ratings"
+        file_name    = "#{short}_ratings.#{extension}"
+        content_type = case extension
+          when 'zip' then "application/zip"
+          when 'csv' then "text/csv"
+          else "text/plain"
+        end
+        if type == "published"
+          rating_list = RatingList.first
+        else
+          rating_list = nil
+        end
+        data         = File.open(file, "r", encoding: "ASCII-8BIT") { |f| f.read }
+        download = Download.where(comment: comment, file_name: file_name, content_type: content_type, rating_list: rating_list).first
+        if download
+          action = "updated"
+          download.data = data
+          download.save!
+        else
+          action = "created"
+          download = Download.create!(comment: comment, file_name: file_name, content_type: content_type, rating_list: rating_list, data: data)
+        end
+        action
     end
   end
 end
