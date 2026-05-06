838a9c6dcabe47e694c7ece68a6fbae98ad16a60
murrayben <dayintoknight@gmail.com>
2026-05-06 16:16:31 +0100

    added helper function for the query (similar to top juniors and seniors) and controller entry

---
+++ app/presenters/icu_ratings/women.rb
@@ -0,0 +1,28 @@
+module IcuRatings
+  class Women
+    attr_reader :list, :club
+
+    def initialize(params)
+      @club = params[:club]
+      @list = params[:list]
+    end
+
+    def list
+      @list ||= IcuRating.unscoped.maximum(:list)
+    end
+
+    def ratings
+      return @ratings if @ratings
+      return [] unless available?
+      @ratings = IcuRating.unscoped.order("rating DESC").includes(:icu_player).references(:icu_player)
+      @ratings = @ratings.where(list: list).where("icu_players.fed = 'IRL' OR icu_players.fed IS NULL")
+      @ratings = @ratings.where("icu_players.gender = 'F'") # Filters for women
+      @ratings = @ratings.where("icu_players.club = ?", @club) if @club.present?
+      @ratings
+    end
+
+    def available?
+      list ? true : false
+    end
+  end
+end
